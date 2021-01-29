3f939df726a44f7bc2eaf5c27ad1efd093938372
Adrian W Nicholls <welfitt@gmail.com>
2021-01-29 18:10:05 +0000

    more tweeks

--- setup.bash
+++ setup.bash
@@ -4,6 +4,13 @@ then
 	echo "This needs to be run as root aborting"
 	exit 1
 fi
+
+if grep -q Hyper-V /var/log/messages
+then
+	echo "Setting screen resolution to something a little more user friendly"
+	grubby --update-kernel=ALL --args="video=hyperv_fb:1920x1080"	
+fi
+
 if ! groups welf | grep -q  wheel 
 then
 	echo "Adding welf to the wheel group and setting up sudo"
@@ -13,7 +20,6 @@ then
 fi
 
 echo "Making SELinux permissive"
-
 sed -i 's/SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
 
 echo "Turning on command line vi editing"
